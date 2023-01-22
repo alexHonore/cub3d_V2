1926d7bbf2b40041965428b6d8630ab68e102d35
Alex Nshimiyimana <alexnshimiyimana@MacBook-Pro-de-Alex.local>
2023-01-22 13:15:58 -0500

    test codam

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: alexnshimiyimana <alexnshimiyimana@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 14:14:48 by anshimiy          #+#    #+#             */
-/*   Updated: 2023/01/20 21:05:00 by alexnshimiy      ###   ########.fr       */
+/*   Updated: 2023/01/21 22:54:53 by alexnshimiy      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,50 +31,45 @@ typedef struct mlx
 */
 // Written by Bruh
 
+#include "../includes/mlx/include/MLX42/MLX42.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "../includes/mlx/include/MLX42/MLX42.h"
+#include <memory.h>
 #define WIDTH 256
 #define HEIGHT 256
 
-// Exit the program as failure.
-static void ft_error(void)
-{
-	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
-	exit(EXIT_FAILURE);
-}
+mlx_image_t	*g_img;
 
-// Print the window width and height.
-static void ft_hook(void* param)
+void	hook(void *param)
 {
-	const mlx_t* mlx = param;
+	mlx_t	*mlx;
 
-	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
+	mlx = param;
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		g_img->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		g_img->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		g_img->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		g_img->instances[0].x += 5;
 }
 
 int32_t	main(void)
 {
+	mlx_t	*mlx;
 
-	// MLX allows you to define its core behaviour before startup.
-	mlx_set_setting(MLX_MAXIMIZED, true);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
+	mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true);
 	if (!mlx)
-		ft_error();
-
-	/* Do stuff */
-
-	// Create and display the image.
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		ft_error();
-
-	// Even after the image is being displayed, we can still modify the buffer.
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
-
-	// Register a hook and pass mlx as an optional param.
-	// NOTE: Do this before calling mlx_loop!
-	mlx_loop_hook(mlx, ft_hook, mlx);
+		exit(EXIT_FAILURE);
+	g_img = mlx_new_image(mlx, 128, 128);
+	memset(g_img->pixels, 255, g_img->width * g_img->height * sizeof(int));
+	mlx_image_to_window(mlx, g_img, 0, 0);
+	mlx_loop_hook(mlx, &hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
